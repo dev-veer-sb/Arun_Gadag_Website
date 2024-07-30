20ea5a922b82e30cee29d4d8b18e7f03a305ab6a
dev-veer-sb <veersb97@gmail.com>
2024-07-30 16:24:54 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className='fs-24'>
+        Welcome
+        </p>
+        <div style={{textDecoration:'underline'}}
+        >
+          ARUN GADAG WEBSITE
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
